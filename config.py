f4533d57614421991ef06f90da559f03b2871c58
JOEL TITO <157946811+JO-Techs@users.noreply.github.com>
2025-04-01 20:56:30 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+import os
+
+GITHUB_APP_ID = os.getenv("GITHUB_APP_ID", "your_app_id")
+GITHUB_PRIVATE_KEY = os.getenv("GITHUB_PRIVATE_KEY", "your_private_key")
+GITHUB_WEBHOOK_SECRET = os.getenv("GITHUB_WEBHOOK_SECRET", "your_webhook_secret")
+GITHUB_REPO = "your-username/your-repo"
+GITHUB_TOKEN = os.getenv("GITHUB_TOKEN", "your_github_token")
